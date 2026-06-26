90e50c247708c333cf4c163d6bafc843cd51ff6f
ParthRKhakre <parthrkhakre@gmail.com>
2026-06-26 23:15:16 +0530

    solidity

--- TokenMarketplace/TokenMarketplace.sol
+++ TokenMarketplace/TokenMarketplace.sol
@@ -1,7 +1,8 @@
-// SPDX-License-Identifier : GPL-3.0
-pragma solidity >=0.7.0 <0.9.0;
+//SPDX-License-Identifier : GPL-3.0
 
+pragma solidity >=0.7.0 <0.9.0;
 import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import {OrderInfo} from "./types/Trade.sol";
 
 contract TokenMarketplace{
 
@@ -9,13 +10,16 @@ contract TokenMarketplace{
     uint private reservedOrderedTokens;
     IERC20 public RAT_Token;
 
+    mapping(uint256 => OrderInfo) private orders;
+    uint256 private nextOrderId;
+
     constructor(address _RAT_Token){
         RAT_Token = IERC20(_RAT_Token);
     }
 
-
     error TokenMarketplace_ZeroNumberOfTokens(uint numberOfTokens);
     error TokenMarketplace_InsufficientEthPayment(uint expectedPayment,uint actualPayment);
+    error TokenMarketplace_InsufficientTokenBalance(uint expectedToken,uint actualToken);
 
     function _isNumberOfTokensZero(uint numberOfTokens)internal pure{
         if(numberOfTokens == 0){
@@ -36,73 +40,23 @@ contract TokenMarketplace{
     function buyTokensFromMarketplace(uint256 numberOfTokens) external payable{
         _isNumberOfTokensZero(numberOfTokens);
         _checkEthPayment(numberOfTokens);
+        if(_getRAT_TokenTokenBalance() < numberOfTokens){
+            uint balance = _getRAT_TokenTokenBalance();
+            revert TokenMarketplace_InsufficientTokenBalance(numberOfTokens,balance);
+        }
 
+        RAT_Token.transfer(msg.sender,numberOfTokens);
     }
 
+    function createSellOrder(uint256 numberOfTokensToSell) external{
+            OrderInfo memory order = OrderInfo({
+                orderId: nextOrderId,
+                seller: msg.sender,
+                numberOfTokens:numberOfTokensToSell,
+                isActive:true
+            });
 
-}                                                                                                                           /*
-
-
-Avoid using global data if require make global data constant 
-Always write constant variable in Capital letters.
-
-By default undefined variable are assigned with default value.
-By default variable are internal in nature in solidity.
-Variable declared with public keyword automatically creates a getter function.
-
-We can decide the capacity of variable meaning what range of value it can store.
-e.g. uint8,uint16,uint24,...,uint256.
-As capacity of variable increases the GAS required also increases.
-
-KEYWORD(access modifiers) - public,private,external,internal.
-
-solidity bydefault used "wei" as its unit
-
-variable : 
-1.state are stored in storage area 
-      they are declared outside the functions
-      gas is required
-      Permenently stored over blockchain
-2.local are declared inside the functions
-
-External function visibility type that forms part of the smart contract's 
-interface, meaning it can only be called from outside the contract
-
-Internal function that can only be called from within the current contract 
-or any contracts that inherit from it
-
-payable keyword is used to indicate that an address or a function 
-can receive and handle Ether (the native currency)
-
-anything that changing the state of blockchain is called transaction
-
-revert() is an error-handling mechanism that aborts the current 
-transaction execution, undoes all state changes (restoring the 
-contract's state to how it was before the call), and refunds all 
-unused gas to the transaction sender
-
-msg.value is a global variable that represents the amount of Wei 
-(the smallest denomination of Ether) sent to a smart contract along 
-with a function call.
-
-public function change more. below is the charge chart : 
-public > external > internal > private
-
-view - read state variable then function is view 
-pure - no read no write from the function
-payable - when function perform eth transaction
-If write then we dont use any of the above
-
-constructor - special function that is the first function to be 
-called automatically when we deploy contract used to initialize 
-value 
-
-import in solidity 
-import {name_of_library} from "source";
-
-interface is a blueprint that defines the function signatures (name, parameters, and return types) 
-of a smart contract without implementing the code itself. interface can be modified according to requirement.
-
-OpenZeppelin is the gold standard, open-source library for building secure smart contracts in Solidity
-
-*/
+            orders[nextOrderId] = order;
+            nextOrderId++;
+    }
+}
---
+++ TokenMarketplace/types/Trade.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier : GPL-3.0
+struct OrderInfo{
+    uint256 orderId;
+    address seller;
+    uint256 numberOfTokens;
+    bool isActive;
+}
